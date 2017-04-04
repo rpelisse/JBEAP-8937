ff14d8ad18f3ccb81793a8595485d7619de1a1ba
Romain Pelisse <belaran@gmail.com>
2017-04-04 15:25:53 +0200

    enhance bash syntax for more script robustness

--- job.sh
+++ job.sh
@@ -8,16 +8,12 @@ if [ "${MYTESTIP_1}x" == "x" ] || [ "${MYTESTIP_2}x" == "x" ] || [ "${MCAST_ADDR
 fi
 
 
-unzip -q ${HUDSON_STATIC_ENV}/eap/${EAP_VERSION}/jboss-eap-${EAP_VERSION}-src.zip
-cd jboss-eap-${EAP_VERSION:0:3}-src
-WORKSPACE=`pwd`
-unzip -q ${HUDSON_STATIC_ENV}/eap/${EAP_VERSION}/jboss-eap-${EAP_VERSION}-testsuite-local-repository.zip
-export MAVEN_REPO_LOCAL=${WORKSPACE}/eap-local-maven-repository
-
-# so that we know what we are using here
-# sh --version
-
+unzip -q "${HUDSON_STATIC_ENV}/eap/${EAP_VERSION}/jboss-eap-${EAP_VERSION}-src.zip"
+cd "jboss-eap-${EAP_VERSION:0:3}-src"
+WORKSPACE=$(pwd)
+unzip -q "${HUDSON_STATIC_ENV}/eap/${EAP_VERSION}/jboss-eap-${EAP_VERSION}-testsuite-local-repository.zip"
+export MAVEN_REPO_LOCAL="${WORKSPACE}/eap-local-maven-repository"
 
 # build EAP and testsuite using OOB build scripts
-bash -x  ./build.sh -B -llr -Dmaven.repo.local=$MAVEN_REPO_LOCAL -fae -DskipTests
-bash -x  ./integration-tests.sh -B -llr -Dmaven.repo.local=$MAVEN_REPO_LOCAL -fae -DskipTests
+bash -x  ./build.sh -B -llr -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -fae -DskipTests
+bash -x  ./integration-tests.sh -B -llr -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -fae -DskipTests
